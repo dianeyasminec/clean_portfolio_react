192d0b73500878fe651af955f3475790bf07c21e
Diane <dianeyasminec60@gmail.com>
2022-12-15 15:17:54 -0500

    update

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Footer = () => (
   <footer className='footer'>
     <a
-      href='https://github.com/dianeyasminec/clean_portfolio_react'
+
       className='link footer__link'
     >
       Created By Diane Compaoré

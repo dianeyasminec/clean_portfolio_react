7f7a44ee9f414ef195c3437c06dbd8325a36aaee
Diane <dianeyasminec60@gmail.com>
2022-12-16 09:44:38 -0500

    update

--- src/portfolio.js
+++ src/portfolio.js
@@ -22,7 +22,7 @@ const header = {
     {
       name: 'Project 1',
       description:
-        'Created an E-commerce application from scratch that displays a list of soaps from a db.json file. Users are able to add to the cart, delete from the cart, and update the total price by increasing/decreasing the number of items.',
+        '●	Developed an E-commerce website that displays homemade soaps. The technologies used were React, Node, CSS, HTML, and JavaScript. Improve navigation between components with routers and connect the front-end and back-end via JSON file. Users can add to cart, delete from the cart, and update the total price by increasing or decreasing the number of items.',
       stack: [ 'React', 'Bootstrap'],
       sourceCode: 'https://github.com/dianeyasminec/E-commerce-with-React.js',
       livePreview: 'https://e-commerce-with-react-js.vercel.app/',
@@ -30,7 +30,7 @@ const header = {
     {
       name: 'Project 2',
       description:
-        'Developed a song lyrics app that allows users to search through a fetch API for any artist, then display 10 popular songs of the artist. Routers are integrated into the application to facilitate the navigation between the components.',
+        "	Recreated and improved the Genius website, which is the world’s biggest collection of song lyrics and musical knowledge.	Implemented the front- end using React, the site fetches data from the search API and uploads it on the page when the user enters the artists name. Integrated lyrics search API to search for, filter and display 10 of the design artist's most popular songs. Utilized routers to improve navigation between components",
       stack: ['React', 'Bootstrap'],
       sourceCode: 'https://github.com/dianeyasminec/Lyrics_generator_React',
       livePreview: 'https://lyrics-generator-react.vercel.app/',
@@ -38,7 +38,7 @@ const header = {
     {
       name: 'Project 3',
       description:
-        'Single page application built entirely with react.js and Bootstrap 5. Biography of author Chimamanda Ngozi Adichie and her popular books.',
+        'Generate a single page website built with React and Bootstrap. Biography of author Chimamanda Ngozi Adichie and her popular books displayed through a JavaScript method.',
       stack: ['React', 'Bootstrap'],
       sourceCode: 'https://github.com/dianeyasminec/Chimamanda-Ngozi-Adichie-',
       livePreview: 'https://chimamanda-ngozi-adichie.vercel.app/',
